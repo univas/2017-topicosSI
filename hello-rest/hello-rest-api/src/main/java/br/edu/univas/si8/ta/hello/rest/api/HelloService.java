d48baf0e4cb77e630f74210b108c2cd9940edc63
Marcio Azevedo <mecva.mecva@gmail.com>
2017-09-11 20:44:14 -0300

    Adding some html pages to our web app.

--- hello-rest/hello-rest-api/src/main/java/br/edu/univas/si8/ta/hello/rest/api/HelloService.java
+++ hello-rest/hello-rest-api/src/main/java/br/edu/univas/si8/ta/hello/rest/api/HelloService.java
@@ -1,10 +1,11 @@
 package br.edu.univas.si8.ta.hello.rest.api;
 
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/service")
@@ -15,9 +16,9 @@ public interface HelloService {
 	@Produces(MediaType.TEXT_HTML)
 	String sayHello(@PathParam("name") String name);
 
-	@GET
+	@POST
 	@Path("/sum")
 	@Produces(MediaType.APPLICATION_JSON)
-	Result sum(@QueryParam("a") int a, @QueryParam("b") int b);
+	Result sum(@FormParam("a") int a, @FormParam("b") int b);
 
 }
