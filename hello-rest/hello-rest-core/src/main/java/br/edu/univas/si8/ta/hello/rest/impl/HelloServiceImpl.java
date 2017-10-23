27638251fe8f8d478da63e30f37ae036794cdaae
Marcio Azevedo <mecva.mecva@gmail.com>
2017-10-23 21:05:13 -0200

    Accessing EJB from REST service.

--- hello-rest/hello-rest-core/src/main/java/br/edu/univas/si8/ta/hello/rest/impl/HelloServiceImpl.java
+++ hello-rest/hello-rest-core/src/main/java/br/edu/univas/si8/ta/hello/rest/impl/HelloServiceImpl.java
@@ -1,9 +1,17 @@
 package br.edu.univas.si8.ta.hello.rest.impl;
 
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+
+import br.edu.univas.si8.ta.hello.ejb.interfaces.Calculator;
 import br.edu.univas.si8.ta.hello.rest.api.HelloService;
 import br.edu.univas.si8.ta.hello.rest.api.Result;
 
+@RequestScoped
 public class HelloServiceImpl implements HelloService {
+
+	@EJB(mappedName = "java:app/hello-rest-ejb-0.1-SNAPSHOT/CalculatorBean!br.edu.univas.si8.ta.hello.ejb.interfaces.CalculatorRemote")
+	private Calculator calculator;
 
 	@Override
 	public String sayHello(String name) {
@@ -12,7 +20,7 @@ public class HelloServiceImpl implements HelloService {
 
 	@Override
 	public Result sum(int a, int b) {
-		int result = a + b;
+		int result = calculator.sum(a, b);
 		return new Result().withA(a).withB(b).withValue(result);
 	}
 
--- hello-rest/hello-rest-war/src/main/java/br/edu/univas/si8/ta/hello/rest/application/RestApplication.java
+++ hello-rest/hello-rest-war/src/main/java/br/edu/univas/si8/ta/hello/rest/application/RestApplication.java
@@ -1,9 +1,21 @@
 package br.edu.univas.si8.ta.hello.rest.application;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import br.edu.univas.si8.ta.hello.rest.impl.HelloServiceImpl;
+
 @ApplicationPath("/rest")
 public class RestApplication extends Application {
+
+	@Override
+	public Set<Class<?>> getClasses() {
+		Set<Class<?>> classes = new HashSet<>();
+		classes.add(HelloServiceImpl.class);
+		return classes;
+	}
 
 }
